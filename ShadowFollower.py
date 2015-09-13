11565c821018db7f19cce82a7d967cd0b3ca98c0
sevoma <dsd8670@rit.edu>
2015-09-13 17:29:49 -0400

    Log follower uses Pika for RabbitMQ and Pygtail for following logs

--- ShadowFollower.py
+++ ShadowFollower.py
@@ -1,13 +1,19 @@
-from websocket._core import create_connection
 from threading import Thread
-import subprocess
+import pika
+from pygtail import Pygtail
+import json
 from time import sleep
 
 
 class ShadowFollower:
     def __init__(self):
-        self.ws = create_connection('ws://localhost:7777/events')  # this seems to be blocking...
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='loglines')
         self.followlog('/var/log/nginx/access.log', 'nginx')
+        while True:
+            pass
 
     def followlog(self, filename, logtype):
         Thread(target=self.tailhandler,
@@ -16,20 +22,20 @@ class ShadowFollower:
                daemon=True).start()
 
     def tailhandler(self, filename, logtype):
-        p = subprocess.Popen(['tail', '-f', filename], stdout=subprocess.PIPE)
         while True:
-            sleep(0.01)
-            line = str(p.stdout.readline())
-            if line:
+            for line in Pygtail(filename, offset_file=logtype):
                 self.push(line, logtype)
+                print(line)
+                sleep(0.1)
 
     def push(self, event, logtype):
-        if self.ws:
-            self.ws.send((event.encode(), logtype.encode()))
-            result = self.ws.recv()
-            print('Broadcasted: ' % result)
+        if self.channel:
+            jsonbody = json.dumps(event, logtype).encode()
+            self.channel.basic_publish(exchange='',
+                                       routing_key='loglines',
+                                       body=jsonbody)
+            print('Broadcasted: ' % event.encode())
 
 
 if __name__ == '__main__':
-    ws = create_connection('ws://localhost:7777/events')
     app = ShadowFollower()
